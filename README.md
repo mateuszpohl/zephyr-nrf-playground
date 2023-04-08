efd6568c1c37006912a0adcc7695df37d3902035
Mateusz Pohl <mateusz.pohl@scareddragon.org>
2023-04-09 00:48:30 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Zephyr Playground
+
+This repository contains a Zephyr playground for nRF52840 DK dev-board.
---
+++ app/src/main.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/sensor.h>
+
+#include "app_version.h"
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+
+void main(void)
+{
+	printk("Welcome to nRF Playground!");
+}
+
